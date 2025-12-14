98c5975cdf05a232bf3987aee9d8b6ce2c6bd85c
kalyan1501 <114755194+kalyan1501@users.noreply.github.com>
2025-12-14 04:19:51 -0800

    Added StuCamApp.swift

---
+++ StuCam/StuCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  StuCamApp.swift
+//  StuCam
+//
+//  Created by M Kalyan on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct StuCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
